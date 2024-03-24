340d9e3e90ed5a42525ce095e7f7a4f07b222f9b
Taavetl <163862539+Taavetl@users.noreply.github.com>
2024-03-24 15:14:05 +0200

    Add files via upload

---
+++ UserGenderValidation/1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, Taavet!");
+
+
+
+
+Console.WriteLine("Please, sellect your gender (F/M)");
+
+char UserGender = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Please, Enter Your Name:");
+
+string username = Console.ReadLine(); 
+
+if(UserGender == 'm')
+{
+    Console.WriteLine($"Hello, Mr. {username}!");
+}
+else if (UserGender == 'f')
+{
+    Console.WriteLine($"Hello, Mrs. {username}!");
+
+}
+else Console.WriteLine($"Welcome, {username}!");
+
+
+
+
+ 
+
+
+
+
---
+++ UserGenderValidation/age/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Sisestage oma vanus:");
+
+int userage =Int32.Parse(Console.ReadLine());
+
+if (userage < 13)
+{
+    Console.WriteLine("Sa oled liiga noor!");
+}
+else if (userage > 13)
+{
+    Console.WriteLine("Welcome!");
+}
+
